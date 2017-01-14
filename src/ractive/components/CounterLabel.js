dea67cb694d7e9453661ded69aece267097ab582
Drew Miley <dmiley@scottlogic.co.uk>
2017-01-14 20:58:26 +0000

    Deploy to gh-pages

--- src/ractive/components/CounterLabel.js
+++
@@ -1,9 +0,0 @@
-import Ractive from 'ractive';
-
-import template from './views/counter-label.html';
-
-let CounterLabel = Ractive.extend({
-	template
-});
-
-export default CounterLabel;
--- src/riot/tags/counter-label.tag
+++
@@ -1,9 +0,0 @@
-<counter-label>
-
-	<p class='h2'>{this.opts.label}: {this.opts.value}</p>
-
-	<script>
-
-	</script>
-
-</counter-label>
